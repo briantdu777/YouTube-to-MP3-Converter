1764a6806a88eb90769fa20cf3100d8da22a7fc4
Brian Du <briantdu777@gmail.com>
2019-09-17 05:11:24 -0700

    Initial Commit
    This can so far take a single argument, a youtube URL, then convert it into an MP3 file.

---
+++ YT2MP3Converter/youtubeCon.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+import youtube_dl
+import sys
+
+def downloadMp3(URL):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([URL])
+
+URL = sys.argv[1]
+downloadMp3(URL)
