5f8489c394aa22cd40161de3d1e4a18b3f3ea73a
unknown <briantdu777@gmail.com>
2020-09-18 02:03:10 -0700

    completed text, readme, and script all updated

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # YouTube-to-MP3-Converter
 This is a python script to help me streamline my youtube to mp3 converting, so that I can make playlists for the road.
+
+#Requirements:
+    -Python 2.7
+    -Libraries: (Use "python pip install")
+        -youtube.dl (https://pypi.org/project/youtube_dl/)
+        -mutagen (https://mutagen.readthedocs.io/en/latest/index.html)
+
+#How to use it:
+
+Git clone the repository.
+
+Create a text file (i.e. SongList.txt) that has lines organized like this (make sure to deliniate with commas):
+[youtubeURL],[albumArt],[title],[artist],[album]
+
+youtubeURL: the URL in which you want the song downloaded as
+albumArt: the location of the album art, if you place it into the pictures folder, "./pictures/nameOfPicture.jpg"
+title: title of song
+artist: artist of song
+album: ablbum of song
+
+on line 83 of youtubeCon.py: os.rename(filename, "D:/iTunes/Automatically Add to iTunes/{}".format(filename)), you want to change "D:/iTunes/Automatically Add to iTunes/{}" to the directory of your Automatically Add to iTunes folder. Don't forget to save!
+
+Then after that, you can go ahead and run the script with the text file.
+
+youtubeCon.py SongList.txt
+
+It should go through each line and then achieve what you want.
--- YT2MP3Converter/youtubeCon.py
+++ YT2MP3Converter/youtubeCon.py
@@ -18,7 +18,12 @@ def downloadMP3(URL):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(URL)
         filename = ydl.prepare_filename(info)
-        return filename[:-4] + "mp3"
+        #print(filename)
+        #print(filename[len(filename) - 3: ])
+        if(filename[len(filename) - 3: ] == "m4a"):
+            return filename[:-4] + ".mp3"
+        else:
+            return filename[:-4] + "mp3"
 
 def editMetaData(filename, imageSRC, title, artist, album):
     audio = ID3(filename)
@@ -48,7 +53,7 @@ convert is the entire routine:
     -> Move the file to iTunes
 '''
 def convert(song):
-    songInfo = song.split(", ")
+    songInfo = song.split(",")
 
     if len(songInfo) != 5:
         print("USAGE ERROR: [youtubeURL] [albumArt] [title] [artist] [album]")
@@ -63,13 +68,23 @@ def convert(song):
     try:
         #convert YT to MP3
         filename = downloadMP3(URL)
-        os.rename(filename, title + ".mp3")
-        filename = title + ".mp3"
+        print(filename)
+
 
     except:
         print("{} by {} failed to be converted".format(title, artist))
         return title
     
+
+    try:
+        #Change it's name
+        os.rename(filename, title + ".mp3")
+        filename = title + ".mp3"
+
+    except:
+        print("{} by {} failed to change its name".format(title, artist))
+        return title
+
     try:
         #edit meta data
         editMetaData(filename, albumArt, title, artist, album)
@@ -80,7 +95,7 @@ def convert(song):
 
     try:
         #move the file to the iTunes folder
-        os.rename(filename, "C:/Users/lildu/Music/iTunes/iTunes Media/Automatically Add to iTunes/{}".format(filename))
+        os.rename(filename, "C:/Users/theal/Music/iTunes/iTunes Media/Automatically Add to iTunes/{}".format(filename))
         print("{} by {}, is now added to iTunes".format(title, artist))
         emptyString = ""
         return emptyString
